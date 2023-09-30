62cc2e959b9da9728bbb55d5ef005e2670f24fdf
Hudson Dye <hudson.dye@outlook.com>
2023-09-30 17:18:30 -0400

    Added a test file

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -7,6 +7,9 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.SmallIntegerField()
 
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+
 class Booking(models.Model):
     id = models.SmallIntegerField(primary_key=True)
     name = models.CharField(max_length=255)
