02a058ac7ed29481aefee194b0938d6a9595d78a
Hudson Dye <hudson.dye@outlook.com>
2023-12-10 14:30:09 -0500

    Adding all pages to the main branch

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,7 +4,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu/', views.MenuItemView.as_view()),
+    path('menuauth/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
+    path('about/', views.about, name="about"),
+    path('menu/', views.menu, name="menu"),
+    path('reservations/', views.reservations, name="reservations"),
+
+    path('book/', views.book, name="book"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
+    path('bookings/', views.bookings, name="bookings"),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets, permissions
 from .serializers import *
 from .models import Menu
+from datetime import datetime
+from .forms import BookingForm
+from django.core import serializers 
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html')
 
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -19,3 +28,56 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add code for the bookings() view
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def reservations(request):
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'bookings.html',{"bookings":booking_json})
+
+
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = Menu.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
