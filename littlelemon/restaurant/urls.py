3e75011e6c30fb8c66f93427cd6425f760a071c3
Hudson Dye <hudson.dye@outlook.com>
2023-12-10 15:54:41 -0500

    Adding all pages to the main branch

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,13 +4,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menuauth/', views.MenuItemView.as_view()),
+    path('menuitem/', views.MenuItemView.as_view(), name="menuitem"),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
     path('about/', views.about, name="about"),
-    path('menu/', views.menu, name="menu"),
     path('reservations/', views.reservations, name="reservations"),
-
+    path('menu/', views.menu, name="menu"),
     path('book/', views.book, name="book"),
     path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
     path('bookings/', views.bookings, name="bookings"),
